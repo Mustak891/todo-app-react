0ff0aa12c519b0c899b07945125b8a1f8c86bc1f
FIRST_NAME LAST_NAME <mustakahammed891@gmail.com>
2022-07-28 12:34:25 +0530

    ssssss

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   const [showPopup,setShowPopup] = useState(false)
 
   useEffect(() => {
-    axios.get('http://localhost:4000/api/tasks').then(res => {
+    axios.get('https://daytodo.herokuapp.com/api/tasks').then(res => {
       setTodolist(res.data)
     }).catch(err => console.log(err))
   },[])
